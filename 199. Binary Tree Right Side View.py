387e45aa3fe075553d58ed4c6d5ab0ba37c70e3f
s7023369667 <s7023369667@gmail.com>
2021-06-29 15:27:10 +0800

    solution

---
+++ 199. Binary Tree Right Side View.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+from typing import List
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        res = {}
+        def traversal(root, deep):
+            if not root:
+                return
+            # print(root.val)
+            if not deep in res:
+                res[deep] = root.val
+            deep += 1
+            traversal(root.right, deep)
+            traversal(root.left, deep)
+        traversal(root, 1)
+        return list(res.values())
+'''
+[1,2,3,4,null,5,null,6]
+       1
+     /  \
+    2    3
+   / \  / \
+  4  N 5   N
+ /
+6
+[1,3,5,6]
+8(cnt) = 2^3(deep)
+'''
+
