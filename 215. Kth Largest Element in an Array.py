82f59b6a69dd8269dab947f980fddb07b0a92f26
s7023369667 <s7023369667@gmail.com>
2021-07-30 20:26:50 +0800

    solution

---
+++ 215. Kth Largest Element in an Array.py
@@ -0,0 +1,48 @@
+from typing import List
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        return self.quick_sort(nums, 0, len(nums) - 1)[-k]
+
+    def middle_of_three(self, nums, front, end) -> int:
+        ##The goal is to find the best pivot to avoid the worst case.
+        m, f, e = 0, 0, 0
+        middle = (front + end) // 2
+        pivot = nums[end]
+        if pivot > nums[middle]:
+            m = 1
+            pivot = nums[middle]
+        elif pivot > nums[front]:
+            m = 0
+            f = 1
+            pivot = nums[front]
+        else:
+            m = 0
+            f = 0
+            e = 1
+        if m:
+            nums[middle], nums[end] = nums[end], nums[middle]
+        elif f:
+            nums[front], nums[end] = nums[end], nums[front]
+        return nums[end]
+
+    def partition(self, nums, front, end) -> int:
+        pivot = self.middle_of_three(nums, front, end)
+        i = front - 1
+        for j in range(front, end):
+            if nums[j] < pivot:
+                i += 1
+                nums[i], nums[j] = nums[j], nums[i]
+        nums[i + 1], nums[end] = pivot, nums[i + 1]
+        return i + 1
+
+    def quick_sort(self, nums, front, end) -> List[int]:
+        if front < end:
+            pivot_index = self.partition(nums, front, end)
+            self.quick_sort(nums, front, pivot_index - 1)
+            self.quick_sort(nums, pivot_index + 1, end)
+        return nums
+
+
+
