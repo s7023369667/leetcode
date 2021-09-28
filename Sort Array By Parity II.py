d5582dd40d1f23c99595cae461955132ade19164
s7023369667 <s7023369667@gmail.com>
2021-09-28 22:12:01 +0800

    solution

---
+++ Sort Array By Parity II.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sortArrayByParityII(self, nums: List[int]) -> List[int]:
+        odd, even = [], []
+        for n in nums:
+            if n % 2 == 0:
+                even.append(n)
+            else:
+                odd.append(n)
+        ans = []
+        for i in range(len(nums)):
+            if (i % 2) == 0:
+                ans.append(even.pop())
+            else:
+                ans.append(odd.pop())
+        return ans
