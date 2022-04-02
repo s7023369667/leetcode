8961c96b0021b8d9870c4c436212f97c9e100d33
Cheng Po Sheng <50870684+s7023369667@users.noreply.github.com>
2022-04-02 11:03:25 +0800

    Create 680. Valid Palindrome II.c

---
+++ 680. Valid Palindrome II.c
@@ -0,0 +1,32 @@
+
+
+bool isPalindrome(char *s, int *l, int *r){
+    while (*l < *r){
+        if (s[*l] != s[*r]){
+            return false;
+        }
+        (*l)++;
+        (*r)--;
+    }
+    return true;
+    
+}
+
+bool validPalindrome(char * s){
+    int L = 0, R = strlen(s)-1;
+    int remove = 0;
+    
+    if (!isPalindrome(s, &L, &R)){
+        //we could only delete at most once.
+        int forward = L+1;  //delete forward?
+        int backward = R-1; //delete backward?
+        
+        //0:0, 0:1, 1:0 are all accept
+        return isPalindrome(s, &forward, &R) || isPalindrome(s, &L, &backward); 
+        
+    }
+    return true;
+}
+
+
+
