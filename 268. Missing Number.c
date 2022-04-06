b4752e5d44ea26addfd5204d11e19844572be889
Cheng Po Sheng <50870684+s7023369667@users.noreply.github.com>
2022-04-06 21:23:21 +0800

    Create 268. Missing Number.c

---
+++ 268. Missing Number.c
@@ -0,0 +1,30 @@
+
+int cmpfunc(const int* a, const int* b){
+    return *a - *b;
+}
+
+
+int missingNumber(int* nums, int numsSize){
+    qsort(nums, numsSize, sizeof(int), cmpfunc);
+    
+    int missing_num = -1;
+    for (int i=0;i<numsSize;i++){
+        if (missing_num+1 != nums[i]){
+            return missing_num + 1;
+        }else{
+            missing_num = nums[i];
+        }
+    }
+    return numsSize;
+}
+
+
+int missingNumber(int* nums, int numsSize){
+    int missing_num = numsSize;
+    /* 7 ^ 0 = 7
+       7 ^ 7 = 0*/
+    for (int i=0;i<numsSize;i++){
+        missing_num = missing_num ^ (nums[i] ^ i);
+    }
+    return missing_num;
+}
