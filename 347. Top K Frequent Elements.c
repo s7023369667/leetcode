cb4f50a3897b13409e42b49e916e4716aed85611
Cheng Po Sheng <50870684+s7023369667@users.noreply.github.com>
2022-04-09 13:11:50 +0800

    Update 347. Top K Frequent Elements.c

--- 347. Top K Frequent Elements.c
+++ 347. Top K Frequent Elements.c
@@ -1,58 +1,44 @@
-
-
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
 
 struct info {
     int val;
-    int freq;
+    int times;  
 };
 
 int cmpfunc1(const int* a, const int* b){
     return *a - *b;
 }
 
-int cmpfunc2(const struct info* a, const struct info* b){    
-    return b->freq - a->freq;
+int cmpfunc2(const struct info* a, const struct info* b){
+    return b->times - a->times;
 }
-
 
 int* topKFrequent(int* nums, int numsSize, int k, int* returnSize){
-    *returnSize = k;
-    int *res = (int*)malloc(sizeof(int)*(*returnSize));
-    if (numsSize == 1){
-        return nums;
-    }
     qsort(nums, numsSize, sizeof(int), cmpfunc1);
-    struct info dir[numsSize];
-    memset(dir, 0, sizeof(struct info)*numsSize); //make hashable
-    int count = 1;
-    dir[0].val = nums[0];
-    dir[0].freq++;
+    struct info table[numsSize];
+    memset(table, 0, sizeof(struct info)*numsSize);
+    int prev = nums[0];
+    table[0].val = prev;
+    table[0].times++;
+    int j = 0;
     for (int i=1;i<numsSize;i++){
-        if (dir[count-1].val == nums[i]){
-            dir[count-1].freq++;
-        }else{
-            count++;
-            dir[count-1].val = nums[i];
-            dir[count-1].freq++;
-        }
-    }    
-    // for (int i=0;i<numsSize;i++){
-    //     printf("%d:%d\n", d[i].val, d[i].freq);
-    // }
-    qsort(dir, numsSize, sizeof(dir[0]), cmpfunc2);
-    for (int i=0;i<*returnSize;i++){
-        res[i] = dir[i].val;
+       if (prev == nums[i]){
+           table[j].times++;
+       }else{
+           prev = nums[i];
+           j++;
+           table[j].val = prev;
+           table[j].times++;
+       }
     }
+    qsort(table, numsSize, sizeof(struct info), cmpfunc2);
+    *returnSize = k;
+    int *res = malloc(sizeof(int)*(*returnSize));
+    for (int i=0;i<*returnSize;i++){
+        res[i] = table[i].val;
+    }
+    
     return res;
 }
-
-
-
-
-
-
-
-
