df882a2b4eb6105d957dd011eb43f5b3c19cedd3
Cheng Po Sheng <50870684+s7023369667@users.noreply.github.com>
2022-04-15 16:46:55 +0800

    Update 213. House Robber II.c

--- 213. House Robber II.c
+++ 213. House Robber II.c
@@ -7,18 +7,18 @@ int rob(int* nums, int numsSize){
         return nums[0];
     }
     int path_a = 0, path_b = 0;
-    int pre = 0;
+    int prepre = 0;
     for (int i=0;i<numsSize-1;i++){
-        int tmp = path_a;
-        path_a = MAX(path_a, pre+nums[i]);
-        pre = tmp;
+        int pre = path_a;
+        path_a = MAX(path_a, prepre+nums[i]);
+        prepre = pre;
     }
     
-    pre = 0;
+    prepre = 0;
     for (int i=1;i<numsSize;i++){
-        int tmp = path_b;
-        path_b = MAX(path_b, pre+nums[i]);
-        pre = tmp;
+        int pre = path_b;
+        path_b = MAX(path_b, prepre+nums[i]);
+        prepre = pre;
     }
     return path_a > path_b ? path_a : path_b;
 }
