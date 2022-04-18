7a225967459d3491f0fd6bf5d5017c1d35404ca3
Cheng Po Sheng <50870684+s7023369667@users.noreply.github.com>
2022-04-18 11:53:07 +0800

    Create 230. Kth Smallest Element in a BST.c

---
+++ 230. Kth Smallest Element in a BST.c
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+struct TreeNode* countNodes(struct TreeNode* root, int* cnt){
+    if (!root){
+        return NULL;
+    }
+    *cnt += 1;
+    countNodes(root->left, cnt);
+    countNodes(root->right, cnt);
+    return root;
+}
+
+struct TreeNode* inorder(struct TreeNode* root, int** order, int* idx, int k){
+    if (!root){
+        return;
+    }
+    inorder(root->left, order, idx);
+    (*order)[*idx] = root->val;
+    *idx += 1;
+    inorder(root->right, order, idx);
+    return root;
+}
+
+
+int kthSmallest(struct TreeNode* root, int k){
+    int cnt = 0;
+    countNodes(root, &cnt);
+    int *order = malloc(sizeof(int)*cnt);
+    int idx = 0;
+    inorder(root, &order, &idx);
+    
+    return order[k-1];
+}
+
+void inorder(struct TreeNode* root, int* idx, int k,struct TreeNode* res){
+    if (!root){
+        return;
+    }
+    inorder(root->left, idx, k, res);
+    *idx += 1;
+    if (*idx == k && res == NULL){
+        res->val = root->val;
+        return;
+    }
+    inorder(root->right, idx, k, res);
+    return;
+}
+
+
+int kthSmallest(struct TreeNode* root, int k){
+    int idx = 0;
+    struct TreeNode* res = malloc(sizeof(struct TreeNode));
+    res->val = -1;
+    inorder(root, &idx, k, res);
+    return res->val;
+}
