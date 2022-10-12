306d179e07ac9fe3664080a9e17249f0b04a8266
Cheng Po Sheng <50870684+s7023369667@users.noreply.github.com>
2022-10-12 15:38:19 +0800

    Update 976. Largest Perimeter Triangle.cpp

--- 976. Largest Perimeter Triangle.cpp
+++ 976. Largest Perimeter Triangle.cpp
@@ -1,3 +1,4 @@
+//method1
 class Solution {
 public:
     int largestPerimeter(vector<int>& nums) {
@@ -14,5 +15,25 @@ public:
             }
         }
         return maxx_perimeter;
+    }
+};
+
+//method2
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        
+        sort(nums.begin(), nums.end(), greater()); 
+        
+        for (int i=0;i<nums.size()-2;i++){
+            
+            if (nums[i] < nums[i+1] + nums[i+2]){
+                
+                return nums[i]+nums[i+1]+nums[i+2];
+            }
+        }
+        
+        return 0;
+        
     }
 };
