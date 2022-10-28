490068ecd36b34d044257471a583ecde180c2e6f
Cheng Po Sheng <50870684+s7023369667@users.noreply.github.com>
2022-10-28 23:21:15 +0800

    Create 49. Group Anagrams.cpp

---
+++ 49. Group Anagrams.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        unordered_map<string, vector<string>> ump;
+        for (int i=0;i<strs.size();i++){
+            string s = strs[i];
+            sort(strs[i].begin(), strs[i].end()); //!!
+            ump[strs[i]].push_back(s);
+        }
+        for (auto m:ump){
+            ans.push_back(m.second);
+        }
+        return ans;
+    }
+};
