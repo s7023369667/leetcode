72fb92ab363aefcfec9f2c5919033fab12f62137
Cheng Po Sheng <50870684+s7023369667@users.noreply.github.com>
2022-10-30 21:56:52 +0800

    Create 203. Remove Linked List Elements.cpp

---
+++ 203. Remove Linked List Elements.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if (!head){
+            return nullptr;
+        }
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr){
+            if (curr->val == val){
+                if (!prev){
+                    head = head->next;
+                }else{
+                    prev->next = curr->next;
+                }
+            }else{
+                prev = curr;
+            }
+            curr = curr->next;
+        }
+        return head;
+    }
+};
